0a8a5ea85cb2ccc453a4de7e511f27db35892ff4
Alexander Mazurov <alexander.mazurov@gmail.com>
2010-03-17 11:14:06 +0100

    Add one more odin trg

--- rundb_django/rundb/views.py
+++ rundb_django/rundb/views.py
@@ -1,11 +1,11 @@
 import datetime
-from django.template import RequestContext,loader
+from django.template import RequestContext, loader
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest
 from django.utils import simplejson
 from django.shortcuts import render_to_response, get_object_or_404
 from django.db.models import Count
 from django.contrib.auth.decorators import login_required
-from rundb_django.rundb.models import Rundbruns,Rundbfiles
+from rundb_django.rundb.models import Rundbruns, Rundbfiles
 from rundb_django.rundb.search_form import SearchForm, ApiForm
 
 
@@ -13,26 +13,26 @@ from rundb_django.rundb.search_form import SearchForm, ApiForm
 def search_form(request=None):
   partitions = []
   for partition in Rundbruns.all_partitions():
-    partitions.append((partition,partition))
+    partitions.append((partition, partition))
     
   runtypes = []
   for runtype in Rundbruns.all_runtypes():
-    runtypes.append((runtype,runtype))
+    runtypes.append((runtype, runtype))
   
   destinations = []
   for destination in Rundbruns.all_destinations():
-    destinations.append((destination,destination))
+    destinations.append((destination, destination))
   
   activities = []
   for activity in Rundbruns.all_activities():
-    activities.append((activity,activity))
+    activities.append((activity, activity))
   
-  return SearchForm(request.user,partitions,runtypes,destinations,activities,request.REQUEST) 
+  return SearchForm(request.user, partitions, runtypes, destinations, activities, request.REQUEST) 
   
 
 def index(request):
     return render_to_response('rundb/rundb_index.html',
-      {'form':search_form(request)},context_instance=RequestContext(request))
+      {'form':search_form(request)}, context_instance=RequestContext(request))
 
 def redirect(request):
   return HttpResponseRedirect("/")
@@ -70,9 +70,9 @@ def maintable(request):
 
       
       if form.cleaned_data['startdate']:
-        starttime = datetime.datetime.combine(form.cleaned_data['startdate'],datetime.time.min)
+        starttime = datetime.datetime.combine(form.cleaned_data['startdate'], datetime.time.min)
         if form.cleaned_data['starttime']:
-          starttime = datetime.datetime.combine(starttime,form.cleaned_data['starttime'])
+          starttime = datetime.datetime.combine(starttime, form.cleaned_data['starttime'])
         runs = runs.filter(starttime__gte=starttime)
       else:
         if form.cleaned_data['starttime']:
@@ -80,13 +80,13 @@ def maintable(request):
           runs = runs.filter(starttime__gte=starttime)
       
       if form.cleaned_data['enddate']:
-        endtime = datetime.datetime.combine(form.cleaned_data['enddate'],datetime.time.max)
+        endtime = datetime.datetime.combine(form.cleaned_data['enddate'], datetime.time.max)
         if form.cleaned_data['endtime']:
-          endtime = datetime.datetime.combine(endtime,form.cleaned_data['endtime'])
+          endtime = datetime.datetime.combine(endtime, form.cleaned_data['endtime'])
         runs = runs.filter(endtime__lte=endtime)
       else:
         if form.cleaned_data['endtime']:
-          endtime = datetime.combine(datetime.now(),form.cleaned_data['endtime'])
+          endtime = datetime.combine(datetime.now(), form.cleaned_data['endtime'])
           runs = runs.filter(endtime__gte=endtime)
 
     tpl = loader.get_template('rundb/rundb_maintable.html')
@@ -94,17 +94,17 @@ def maintable(request):
       {'runs': runs.all().order_by('-runid')[0:form.cleaned_data['onpage']]})
     json = simplejson.dumps(tpl.render(ctx))
   else:
-    json = simplejson.dumps("form not valid"+str(form.errors))
+    json = simplejson.dumps("form not valid" + str(form.errors))
 
   return HttpResponse(json, mimetype='application/json')
 
 
-def files(request,runid,page=1):
+def files(request, runid, page=1):
   #--------------------------------------------------------------------------------------
-  onpage=10
-  page=int(page)
+  onpage = 10
+  page = int(page)
   #--------------------------------------------------------------------------------------
-  if page==1:
+  if page == 1:
     tplfile = 'rundb/rundb_files.html'
   else:
     tplfile = 'rundb/rundb_files_rows.inc.html'
@@ -114,42 +114,42 @@ def files(request,runid,page=1):
   #--------------------------------------------------------------------------------------
   # Context
   #--------------------------------------------------------------------------------------
-  context = {'run':run,'files': run.rundbfiles_set.all()[(page-1)*onpage:page*onpage]}
-  if run.rundbfiles_set.count() > page*onpage:
-    context['next'] = page+1
+  context = {'run':run, 'files': run.rundbfiles_set.all()[(page - 1) * onpage:page * onpage]}
+  if run.rundbfiles_set.count() > page * onpage:
+    context['next'] = page + 1
   #--------------------------------------------------------------------------------------
-  ctx = RequestContext(request,context)
+  ctx = RequestContext(request, context)
   #--------------------------------------------------------------------------------------
   return HttpResponse(simplejson.dumps(tpl.render(ctx)),
                                                 mimetype='application/json');
 
 @login_required
-def file_pin(request,fileid):
+def file_pin(request, fileid):
   file = Rundbfiles.objects.get(pk=fileid)
   file.pin(request.user)
   file.save();
   result = simplejson.dumps({'refcount':file.refcount})
-  return HttpResponse(result,mimetype='application/json')
+  return HttpResponse(result, mimetype='application/json')
 
-def file_log(request,fileid):
+def file_log(request, fileid):
   file = Rundbfiles.objects.get(pk=fileid)
   return render_to_response('rundb/rundb_file_log.html',
-      {'file':file},context_instance=RequestContext(request))
+      {'file':file}, context_instance=RequestContext(request))
 
-def run(request,runid):
+def run(request, runid):
   run = Rundbruns.objects.get(pk=runid)
   odins = []
-  for i in range(7):
-    odins.append(getattr(run,'odin_trg_'+str(i)))
+  for i in range(8):
+    odins.append(getattr(run, 'odin_trg_' + str(i)))
 
   return render_to_response('rundb/rundb_run.html',
-      {'single':True, 'run':run,'runs':[run],'iodins':range(7),'odins':odins},context_instance=RequestContext(request))
+      {'single':True, 'run':run, 'runs':[run], 'iodins':range(8), 'odins':odins}, context_instance=RequestContext(request))
 
-def file(request,fileid):
+def file(request, fileid):
   file = Rundbfiles.objects.get(pk=fileid)
   run = file.run
   return render_to_response('rundb/rundb_file.html',
-      {'single':True, 'file':file,'files':[file],'run':run},context_instance=RequestContext(request))
+      {'single':True, 'file':file, 'files':[file], 'run':run}, context_instance=RequestContext(request))
 
 
 def _api_response(request, result):
@@ -242,7 +242,7 @@ def api_search(request):
      runs = runs.filter(partitionname__exact=form.cleaned_data['partition'])
 
   if form.cleaned_data['destination']:
-     runs = runs.filter(destination__in=(form.cleaned_data['destination'], ))
+     runs = runs.filter(destination__in=(form.cleaned_data['destination'],))
 
   if form.cleaned_data['starttime']:
     runs = runs.filter(starttime__gte=form.cleaned_data['starttime'])
